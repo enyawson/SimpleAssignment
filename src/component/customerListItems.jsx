59059320af76bc2d91ebbc6781269aebcdc1e6ae
Emmanuel Yawson <enyawson33@gmail.com>
2022-05-17 21:27:58 +0000

    tables not done

--- src/component/customerListItems.jsx
+++ src/component/customerListItems.jsx
@@ -34,6 +34,11 @@ export default class customerListItems extends Component {
         selectedList:[],
         quantity:"",
         itemInserted:'',
+        itemAdded:'',
+        itemQuantityAdded:'',
+        employeeSelected:false,
+        customerSelected:true,
+
     };
 
    
@@ -62,32 +67,36 @@ export default class customerListItems extends Component {
 
  }
 
+ submitInsetedData = ()=>{
+     this.setState({
+         itemAdded:'',
+         itemQuantityAdded:'',
+     })
+ }
 
+ //items to be inserted by employeeee
  insertItems=()=>{
     return(
         <form >
         <div class="form-row" style={{justifyContent:'center'}}>
             <div class="form-group col-md-6">
-                <label for="inputEmail4">Item</label>
-                <input type="text" class="form-control"  placeholder="Enter item"/>
+                <label>Item</label>
+                <input type="text" class="form-control" value={this.state.itemAdded} placeholder="Enter item"/>
             </div>
             <div class="form-group col-md-2">
                 <label for="inputPassword4">Quantity</label>
-                <input type="password" class="form-control"  placeholder=""/>
+                <input type="number" class="form-control" value={this.state.itemQuantityAdded}  placeholder=""/>
             </div>
             <div style={{marginTop:30}}>
-                 <button type="submit" class="btn btn-primary">Insert</button>
+                 <button type="submit" class="btn btn-primary" onClick={this.submitInsetedData}>Insert</button>
             </div>
-           
         </div>
       </form>
-    
     );
 
  }
 
- 
-
+   //actions buttons, for employee
     actionButtons=()=>{
         return(
             <div class="btn-group" role="group" aria-label="main group" style={{width:'80%',paddingTop:20, paddingBottom:20}}>
@@ -108,9 +117,59 @@ export default class customerListItems extends Component {
 
     }
 
+    //actions buttons, for customer
+    employeeActionButtons=()=>{
+        return(
+            <div class="btn-group" role="group" aria-label="main group" style={{width:'80%',paddingTop:20, paddingBottom:20}}>
+                <div className="btn-group-lg" role="group" aria-label="First Group" style={{flex:1, backgroundColor:'green', margin:5}}>
+                    <button  style={styleTopButton}  type="button" class="btn  text-white">Insert Items</button>
+                </div>
+                <div className="btn-group-lg text-white" role="group" aria-label="Second Group"  style={{flex:1, backgroundColor:'green', margin:5}}>
+                     <button type="button" class="btn  text-white" style={styleTopButton} >Delete</button>
+                </div>
+                <div className="btn-group-lg" role="group" aria-label="Third group" style={{flex:1, backgroundColor:'green', margin:5}}>
+                     <button style={styleTopButton} type="button" class="btn  text-white">Update</button>
+                </div>
+                <div className="btn-group-lg" role="group" aria-label="fourth group" style={{flex:1, backgroundColor:'green', margin:5}}>
+                     <button style={styleTopButton} type="button" class="btn  text-white">Retrieve</button>
+                </div>
+            </div>
+        )
+
+    }
+      //table of items
+      employeeItemsTable() {
+        return (<div className="container">
+           
+            <p>Please Insert from the list of items available</p>
+            
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Items</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    {
+                    this.state.list.map((item, index)=> 
+                        <tbody key={index} className="Table-body">
+                            <tr>
+                                <td>{item.itemName}</td>
+                                {/* <td>
+                                <button type="button" className="btn btn-success" onClick={this.handleAddToList}>Add</button>
+                                </td> */}
+                            </tr>
+                        </tbody>
+                    )}
+                </table>
+
+        </div>); 
+    }
+
+
+    //table of items
     fillTable() {
         return (<div className="container">
-            <h2>Customer</h2>
            
             <p>Please select from the list of items available</p>
             
@@ -134,18 +193,41 @@ export default class customerListItems extends Component {
                         </tbody>
                     )}
                 </table>
-               
-            
-        </div>);
-  
-       
+        </div>); 
     }
+ userCustomer=()=>{
+     this.setState({
+         customerSelected: true,
+         employeeSelected: false
+     })
+ }
+ userEmployee=()=>{
+    this.setState({
+        customerSelected: false,
+        employeeSelected: true
+    })
+}
+
     render() {
         return(
             <div>
-                <div>{this.actionButtons()}</div>
-                <div>{this.fillTable()}</div> 
-                <div> {this.insertItems()}</div>
+                <div style={{flexDirection:'row', margin:5, marginBottom: 10}}>
+                    <button  style={{marginRight:10}} onClick={this.userCustomer} type="button" class="btn btn-primary btn-lg">EMPLOYEE</button>   
+                    <button  style={{marginLeft:10}}onClick={this.userEmployee} type="button" class="btn btn-primary btn-lg">CUSTOMER</button>                 
+                </div>
+                {this.state.customerSelected == true? <div>
+                    <h2>Customer</h2>
+                    <div>{this.actionButtons()}</div>
+                    <div>{this.fillTable()}</div> 
+                    <div> {this.insertItems()}</div>
+                </div>
+                : 
+                <div>
+                    <h2>Employee</h2>
+                        {this.employeeActionButtons()}
+                        {this.employeeItemsTable()}
+                </div>}
+               
             </div>
            
         );
